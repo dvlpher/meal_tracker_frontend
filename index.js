0e533908b8d978d800a309e8c714873aab07adbf
CelesteSMThomas <celestesmthomas@gmail.com>
2021-04-22 17:57:19 -0700

    created html and js to show meals

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/meals";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getMeals();
+});
+
+function getMeals() {
+  fetch(endPoint)
+    .then((response) => response.json())
+    .then((meals) => {
+      meals.data.forEach((meal) => {
+        const mealMarkup = ` <div data-id=${meal.id}>
+        <img src=${meal.attributes.image_url} height="200" width ="250">
+        <h3>${meal.attributes.name}</h3>
+        <p>${meal.attributes.category.name}</p>
+        <button data-id=${meal.id}>edit</button>
+        </div>
+        <br><br>`;
+        document.querySelector("#meal-container").innerHTML += mealMarkup;
+      });
+    });
+}
